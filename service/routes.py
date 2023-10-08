61ae62386b3a86e791a1c7ba9a5e839d51a7f0c7
yacine-04 <yacineatmani04@gmail.com>
2023-10-08 09:46:23 -0400

    Resolved conflict in service/routes.py

--- service/routes.py
+++ service/routes.py
@@ -8,19 +8,21 @@ from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
 from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
+
+# Two blank lines here
+
 ############################################################
 # Health Endpoint
 ############################################################
-
 
 @app.route("/health")
 def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
+
 ######################################################################
 # GET INDEX
 ######################################################################
-
 
 @app.route("/")
 def index():
@@ -33,16 +35,16 @@ def index():
         ),
         status.HTTP_200_OK,
     )
+
 ######################################################################
 # CREATE A NEW ACCOUNT
 ######################################################################
-
 
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
     """
     Creates an Account
-    This endpoint will create an Account based the data in the body that is posted
+    This endpoint will create an Account based on the data in the body that is posted
     """
     app.logger.info("Request to create an Account")
     check_content_type("application/json")
@@ -56,10 +58,10 @@ def create_accounts():
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
@@ -69,26 +71,22 @@ def list_accounts():
     """
     app.logger.info("Request to list Accounts")
 
-
-
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
 
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
     Reads an Account
-    This endpoint will read an Account based the account_id that is requested
+    This endpoint will read an Account based on the account_id that is requested
     """
-
-
     app.logger.info("Request to read an Account with id: %s", account_id)
 
     account = Account.find(account_id)
@@ -96,10 +94,10 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -109,8 +107,6 @@ def update_accounts(account_id):
     """
     app.logger.info("Request to update an Account with id: %s", account_id)
 
-
-
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
@@ -119,15 +115,13 @@ def update_accounts(account_id):
     account.update()
 
     return account.serialize(), status.HTTP_200_OK
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
-
-
     """
     Delete an Account
     This endpoint will delete an Account based on the account_id that is requested
@@ -139,9 +133,11 @@ def delete_accounts(account_id):
         account.delete()
 
     return "", status.HTTP_204_NO_CONTENT
+
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ##################################################################
+
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
@@ -152,3 +148,5 @@ def check_content_type(media_type):
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
     )
+
+# One blank line at the end of the file
